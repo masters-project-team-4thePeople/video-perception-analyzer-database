7c312942b585627e11438bf9d2fce0ee38cef86a
aryan-jadon <aryan.jadon@sjsu.edu>
2023-04-29 15:58:31 -0700

    Refactored Directory

--- README.md
+++
@@ -1,2 +0,0 @@
-# video-perception-analyzer-database
-Video Perception Analyzer Database API codebase
--- videos_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VideosApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'videos_api'
